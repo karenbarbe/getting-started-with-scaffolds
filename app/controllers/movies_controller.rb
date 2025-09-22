092eb61924855d2d59530d87f85200c8c0482420
Karen Barbé <info@karenbarbe.com>
2025-09-22 13:05:14 -0500

    update #create to render with_errors and change the_movie variable to instance variable

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,17 +18,26 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies", { :notice => "Movie was successfully created." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movie_templates/with_errors"
     end
+  end
+
+  def new
+    render({ :template => "movie_templates/new" })
+  end
+
+  def edit
+    @the_movie = Movie.where(id: params.fetch(:id))[0]
+    render({ :template => "movie_templates/edit" })
   end
 
   def update
@@ -37,7 +46,7 @@ class MoviesController < ApplicationController
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+    the_movie.released = params.fetch("query_released", false)
 
     if the_movie.valid?
       the_movie.save
