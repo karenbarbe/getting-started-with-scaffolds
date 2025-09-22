144e9eb8dcb1f7414d9541c6c2c0e130eec151eb
Karen Barbé <info@karenbarbe.com>
2025-09-22 13:51:00 -0500

    refactor movie creation and update logic; improve error handling and form rendering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,11 +27,12 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to("/movies", { :notice => "Movie was successfully created." })
     else
-      render template: "movie_templates/with_errors"
+      render template: "movie_templates/new"
     end
   end
 
   def new
+    @the_movie = Movie.new
     render({ :template => "movie_templates/new" })
   end
 
@@ -42,17 +43,17 @@ class MoviesController < ApplicationController
 
   def update
     the_id = params.fetch("id")
-    the_movie = Movie.where({ :id => the_id })[0]
+    @the_movie = Movie.where({ :id => the_id })[0]
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully." } )
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies/#{@the_movie.id}", { :notice => "Movie updated successfully." } )
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
+      redirect_to("/movies/#{@the_movie.id}", { :alert => @the_movie.errors.full_messages.to_sentence })
     end
   end
 
